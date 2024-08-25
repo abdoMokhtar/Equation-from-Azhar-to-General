eeec9a697ee441b273df0e5dbc9cd675274df30b
Abdo <abdomokhtar446@gmail.com>
2024-08-25 19:52:15 +0300

    Alert nagative value

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const sum = document.querySelector("input.sum");
 const part = document.querySelector("input.part");
 const button = document.querySelector("button");
@@ -5,8 +6,30 @@ const finalSum = document.querySelector("span.one");
 const final = document.querySelector("span.two");
 
 button.addEventListener("click", function () {
-  let numOne = (((sum.value - part.value) / 440) * 100).toFixed(2);
-  let numTwo = sum.value - part.value;
-  finalSum.textContent = `%${numOne}`;
-  final.textContent = numTwo;
+  if (sum.value > part.value) {
+    let numOne = (((sum.value - part.value) / 440) * 100).toFixed(2);
+    let numTwo = sum.value - part.value;
+    finalSum.textContent = `%${numOne}`;
+    final.textContent = numTwo;
+  } else {
+    function popup() {
+      let creat = document.createElement("div");
+      creat.innerHTML =
+        "<span>برجاء التأكد من أن المجموع الكلي أكبر من مجموع المواد الشرعيه</span";
+      //   -------------------------------------------------------
+      let btn = document.createElement("button");
+      btn.textContent = "X";
+      btn.style.cssText =
+        "background-color: #b30e0e;height: 30px;width: 30px;border-radius: 15px;border: none;color: white;font-size: x-large;position: absolute;top: -17px;right: -15px;display: grid;place-content: center;";
+      creat.appendChild(btn);
+      //   ---------------------------------------------------------
+      creat.style.cssText =
+        "font-size:18px;text-align:center;position: absolute;height: 130px;width: 300px;background-color: rgb(238, 238, 238);display: flex;justify-content: center;align-items: center;flex-direction: column;top: 50%;left: 50%;transform: translate(-50%, -50%);border-radius: 20px;";
+      document.body.appendChild(creat);
+      btn.onclick = function () {
+        creat.remove();
+      };
+    }
+    popup();
+  }
 });
