ce5d3b3c7115f72d24809ae25b9453e05075fd9b
Abdo <abdomokhtar446@gmail.com>
2024-08-25 19:59:57 +0300

    edit number of sum

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const finalSum = document.querySelector("span.one");
 const final = document.querySelector("span.two");
 
 button.addEventListener("click", function () {
-  if (sum.value > part.value) {
+  finalSum.textContent = `%${0}`;
+  final.textContent = 0;
+  if (+sum.value > +part.value) {
     let numOne = (((sum.value - part.value) / 440) * 100).toFixed(2);
     let numTwo = sum.value - part.value;
     finalSum.textContent = `%${numOne}`;
